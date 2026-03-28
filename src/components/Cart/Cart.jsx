6bacd6dd52b78ea637c6e5a78a5d5d274061740d
Saha-Ovi <ovisaha3210@gmail.com>
2026-03-29 02:39:16 +0600

    cart edge case handled

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,12 @@ import { toast } from 'react-toastify';
 const Cart = ({ cart, setCart }) => {
     // console.log(cart);
     const totalPrice = cart.reduce((sum, cartItem) => sum + cartItem.price, 0)
-    const handledCheckout=()=>
-    {
+    const handledCheckout = () => {
         setCart([]);
         toast.success("Proceeded to the checkout ");
     }
-    const handledRemove=(item)=>
-    {
-        const newCart=cart.filter(cartItem=>cartItem.id!==item.id);
+    const handledRemove = (item) => {
+        const newCart = cart.filter(cartItem => cartItem.id !== item.id);
         setCart(newCart);
         toast.success(`${item.title} remove from the card successfully`)
     }
@@ -21,8 +19,15 @@ const Cart = ({ cart, setCart }) => {
             <h2 className='text-5xl font-extrabold'>Your Cart</h2>
             <div className='space-y-6 hover:cursor-pointer'>
 
+
                 {
-                    cart.map(item => <CartCard key={item.id} item={item} setCart={setCart} totalPrice={totalPrice} handledRemove={handledRemove}></CartCard>)
+                    cart.length === 0 ?
+                        <div className='shadow-xl bg-zinc-400 rounded-2xl text-center p-4'>
+                            <h2 className='text-2xl font-extrabold'>Cart is Empty</h2>
+                            <p className='text-xl font-bold'>Please Add item to the cart</p>
+                        </div>
+                        :
+                        cart.map(item => <CartCard key={item.id} item={item} setCart={setCart} totalPrice={totalPrice} handledRemove={handledRemove}></CartCard>)
                 }
             </div>
             <div className='flex justify-between items-center bg-black text-white p-6 rounded-2xl '>
